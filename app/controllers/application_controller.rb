23e286de674d59aa17bb2401bf37e0151c427985
lekova <lina.lekova@gmail.com>
2015-07-24 17:20:01 -0400

    Add listing users if logged in.
    User active_model_serializers to constrain information returned about
    each user. Problem with using explicit serializer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ::ActionController::Serialization
+
+
   before_action :authenticate
 
   attr_reader :current_user
