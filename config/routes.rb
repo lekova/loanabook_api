c98bed1a159aa647d0fe63c5c75a571e655c8294
lekova <lina.lekova@gmail.com>
2015-07-31 09:52:37 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/login' => 'users#login'
 
   resources :users, except: [:new, :edit]
+  resources :books, :loans
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
