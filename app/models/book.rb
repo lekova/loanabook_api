ee8949cc4d37fada276f0ead090024600a3609e8
lekova <lina.lekova@gmail.com>
2015-08-02 16:35:52 -0400

    Fixed in book model for owner_id
    Fixes owner_id relationship
    Adds validation to owner_id
    Adds status enum

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner, class_name: "User"
   belongs_to :loan
 
-  validates :owner_id, presence: true
+  validates :owner, presence: true
+
+  enum status: { available: 0, unavailable: 1, deleted: 3 }
+  # http://www.sitepoint.com/enumerated-types-with-activerecord-and-postgresql/
 end
