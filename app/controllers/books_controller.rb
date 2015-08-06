92fddccd7fe7395a3c63e7445d9c0253747381e3
lekova <lina.lekova@gmail.com>
2015-08-06 19:14:28 -0400

    Remove id from book required parameters

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,7 +44,7 @@ class BooksController < OpenReadController
   private
   def book_params
     # TODO add summary
-    params.require(:book).permit(:id, :title, :author, :year, :price, :url, :status)
+    params.require(:book).permit(:title, :author, :year, :price, :url, :status)
   end
 
   def book_params_with_user_id
