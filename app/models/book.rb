2abd5ad52c552f11e566adcbad65fe7369a288c2
lekova <lina.lekova@gmail.com>
2015-08-07 12:13:59 -0400

    Add has_attached_file to book model
    paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,9 @@ class Book < ActiveRecord::Base
 
   enum status: { available: 0, unavailable: 1, deleted: 3 }
   # http://www.sitepoint.com/enumerated-types-with-activerecord-and-postgresql/
+
+  has_attached_file :image,  #Or whatever you want to call the image you're uploading.
+              :styles => { :medium => "300x300>", :thumb => "100x100>" },
+              :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
