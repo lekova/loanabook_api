497f7f24c11f4c2c825990fff9704650abccdcda
lekova <lina.lekova@gmail.com>
2015-08-07 12:14:49 -0400

    Add belogns_to books in loan model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,7 @@
 class Loan < ActiveRecord::Base
   belongs_to :loaner, class_name: "User"
   belongs_to :borrower, class_name: "User"
+  belongs_to :book
 
   validates :book_id, presence: true
   validates :loaner, presence: true
